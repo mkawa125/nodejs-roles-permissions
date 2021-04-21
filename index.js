aabdbea04831eb8a695a50337f9f17155a69c60e
Dahabu Saidi <dahabusaidi@gmail.com>
2021-04-21 13:52:52 +0300

    Introduce try catch

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const exp = require("express");
 const bp = require("body-parser");
 const {connect} = require("mongoose");
-const {success} = require("consola");
+const {success, error} = require("consola");
 
 // Bring in the app constants
 const {DB, PORT} = require("./config");
@@ -11,7 +11,30 @@ const {DB, PORT} = require("./config");
 const app = exp();
 
 // Connection to database
+const startApp = async() => {
+   try {
+    await connect(DB, {
+        useFindAndModify: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
 
-app.listen(PORT, () => 
-    success({message: `Server started on port ${PORT}`, badge: true})
-);
+    success({
+        message: `Successfully connected with the database \n${DB}`, 
+        badge: true
+    });
+    
+
+    app.listen(PORT, () => 
+        success({message: `Server started on port ${PORT}`, badge: true})
+    );
+   } catch (error) {
+        error({
+            message: `Can not connect with the database \n${error}`, 
+            badge: true
+    });
+    startApp();
+   }
+};
+
+startApp();
