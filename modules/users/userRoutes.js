9c8255e191bf46f32bdf2cbd95fd879da3b38f09
Dahabu Saidi <dahabusaidi@gmail.com>
2021-05-17 23:06:54 +0300

    Add authentication to users routes

--- modules/users/userRoutes.js
+++ modules/users/userRoutes.js
@@ -4,15 +4,16 @@ const router = require("express").Router();
  * Bring in user registration function 
  */ 
 const { getAllUsers, getUserById, updateUser, deleteUser } = require("./userController")
+const { userAuthentication } = require("../auth/authController")
 
 /** 
  * User registration route 
  */
-router.get("/", async (req, res) => { getAllUsers(req.body, res) });
+router.get("/", userAuthentication, async (req, res) => { getAllUsers(req.body, res) });
 
-router.get("/:userId", getUserById);
-router.put("/:userId", updateUser);
-router.delete("/:userId", deleteUser);
+router.get("/:userId", userAuthentication, getUserById);
+router.put("/:userId", userAuthentication,  updateUser);
+router.delete("/:userId", userAuthentication, deleteUser);
 
 
 module.exports = router;
